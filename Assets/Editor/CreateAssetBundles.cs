14e235a270d0a1e6b4861e54ed2f6e87991bd7c4
Kaylee Schwab <schwab.ty@gmail.com>
2024-02-10 16:23:56 -0600

    adding blahaj

---
+++ Assets/Editor/CreateAssetBundles.cs
@@ -0,0 +1,17 @@
+using UnityEditor;
+using System.IO;
+
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = "Assets/AssetBundles";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.Exists(assetBundleDirectory);
+        }
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
+    }
+
+}
